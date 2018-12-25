c60b6063098af5e0d2ee4c6a20500d136e4746ab
devtechsolution <srivastvadevzone@gmail.com>
2018-12-25 10:55:50 +0530

    Initial Commit

---
+++ src/main/java/org/techsolution/app/Spring5appApplication.java
@@ -0,0 +1,14 @@
+package org.techsolution.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5appApplication.class, args);
+	}
+
+}
+
